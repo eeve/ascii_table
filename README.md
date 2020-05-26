f65375bb8fddc924137155edc6f5fa501bf7fc93
eeve <eeve@users.noreply.github.com>
2020-05-26 16:02:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,629 @@
+Ascii Table
+===========
+
+Easy table output for node debugging, but you could probably do more with it, 
+since its just a string.
+
+Table of Contents
+-----------------
+
+* [Usage](#usage)
+* [Example](#usage)
+* [API](#api)
+  - [Static Methods](#static-methods)
+    * [align(direction, val, len, [pad])](#asciitablealigndirection-val-len-pad)
+    * [alignLeft(val, len, [pad])](#asciitablealignleftval-len-pad)
+    * [alignCenter(val, len, [pad])](#asciitablealigncenterval-len-pad)
+    * [alignRight(val, len, [pad])](#asciitablealignrightval-len-pad)
+    * [alignAuto(val, len, [pad])](#asciitablealignautoval-len-pad)
+    * [arrayFill(len, [val])](#asciitablearrayfilllen-val)
+  - [Instance Methods](#instance-methods)
+    * [setBorder([edge], [fill], [top], [bottom])](#instancesetborderedge-fill-top-bottom)
+    * [removeBorder()](#instanceremoveborder)
+    * [setAlign(idx, direction)](#instancesetalignidx-direction)
+    * [setAlignLeft(idx)](#instancesetalignleftidx)
+    * [setAlignCenter(idx)](#instancesetaligncenteridx)
+    * [setAlignRight(idx)](#instancesetalignrightidx)
+    * [setTitle(title)](#instancesettitletitle)
+    * [getTitle()](#instancegettitle)
+    * [setTitleAlign(direction)](#instancesettitlealigndirection)
+    * [setTitleAlignLeft()](#instancesettitlealignleft)
+    * [setTitleAlignCenter()](#instancesettitlealigncenter)
+    * [setTitleAlignRight()](#instancesettitlealignright)
+    * [sort([iterator])](#instancesortiterator)
+    * [sortColumn(idx, [iterator])](#instancesortcolumnidx-iterator)
+    * [setHeading(heading, [...])](#instancesetheadingheading)
+    * [setHeadingAlign(direction)](#instancesetheadingaligndirection)
+    * [setHeadingAlignLeft()](#instancesetheadingalignleft)
+    * [setHeadingAlignCenter()](#instancesetheadingaligncenter)
+    * [setHeadingAlignRight()](#instancesetheadingalignright)
+    * [addRow(row, [...])](#instanceaddrowrow)
+    * [addRowMatrix(rows)](#instanceaddrowmatrixrows)
+    * [setJustify([enabled])](#instancesetjustifyenabled)
+    * [toString()](#instancetostring)
+    * [toJSON()](#instancetojson)
+    * [fromJSON(obj)](#instancefromjsonobj)
+    * [clear()](#instanceclear)
+    * [clearRows()](#instanceclearrows)
+* [Install](#install)
+* [Contributors](#contributors)
+* [License](#license)
+
+Usage
+-----
+
+Deno
+
+```ts
+import AsciiTable, { AsciiAlign } from 'https://deno.land/x/ascii-table/mod.ts';
+
+const table1 = new AsciiTable('Title')
+table1
+  .setHeadingAlign(AsciiAlign.CENTER)
+  .setHeading('#', 'Name', 'Age')
+  .addRow(1, 'Bob', 52)
+  .addRow(2, 'John', 34)
+  .addRow(3, 'Jim', 83)
+
+const table2 = AsciiTable.fromJSON({
+  title: 'Title',
+  heading: [ 'id', 'name' ],
+  rows: [
+    [1, 'Bob'],
+    [1, 'John'],
+    [1, 'Jim']
+  ]
+})
+
+console.log(table1.toString())
+console.log(table2.toString())
+
+```
+
+Example
+-------
+
+Basic usage
+
+```ts
+const table = new AsciiTable('A Title')
+table
+  .setHeading('', 'Name', 'Age')
+  .addRow(1, 'Bob', 52)
+  .addRow(2, 'John', 34)
+  .addRow(3, 'Jim', 83)
+
+console.log(table.toString())
+```
+
+```
+.----------------.
+|    A Title     |
+|----------------|
+|   | Name | Age |
+|---|------|-----|
+| 1 | Bob  |  52 |
+| 2 | John |  34 |
+| 3 | Jim  |  83 |
+'----------------'
+```
+
+We can make a simple table without a title or headings as well.
+
+```ts
+const table = new AsciiTable()
+
+table
+  .addRow('a', 'apple', 'Some longer string')
+  .addRow('b', 'banana', 'hi')
+  .addRow('c', 'carrot', 'meow')
+  .addRow('e', 'elephants')
+
+
+console.log(table.toString())
+```
+
+```
+.------------------------------------.
+| a | apple     | Some longer string |
+| b | banana    | hi                 |
+| c | carrot    | meow               |
+| e | elephants |                    |
+'------------------------------------'
+```
+
+
+API
+---
+
+### Constructor
+
+Table instance creator
+
+* `title` - table title (optional, default `null`)
+* `options` - table options (optional)
+  - `prefix` - string prefix to add to each line on render
+
+***Note:*** If an object is passed in place of the `title`, the `fromJSON` 
+method will be used to populate the table.
+
+Example:
+
+```ts
+const table = new AsciiTable('title')
+```
+
+### Static Methods
+
+#### AsciiTable
+
+#### AsciiTable.fromJSON(AsciiData)
+
+* `title` - table title
+* `heading` - heading array or arguments
+* `rows` - array or arguments of column values
+
+```ts
+const table = AsciiTable.fromJSON({
+  title: 'Title',
+  heading: [ 'id', 'name' ],
+  rows: [ 
+    [ 1, 'Bob' ],
+    [ 2, 'Steve' ] 
+  ] 
+})
+```
+
+#### AsciiTable.align(direction, val, len, [pad])
+
+Shortcut to one of the three following methods
+
+* `direction` - alignment direction (`AsciiAlign.LEFT`, `AsciiAlign.CENTER`, `AsciiAlign.RIGHT`)
+* `val` - string to align
+* `len` - total length of created string
+* `pad` - padding / fill char (optional, default `' '`)
+
+Example:
+
+```js
+table.align(AsciiAlign.LEFT, 'hey', 7) // 'hey    '
+```
+
+
+#### AsciiTable.alignLeft(val, len, [pad])
+
+* `val` - string to align
+* `len` - total length of created string
+* `pad` - padding / fill char (optional, default `' '`)
+
+Example:
+
+```js
+table.alignLeft('hey', 7, '-') // 'hey----'
+```
+
+
+#### AsciiTable.alignCenter(val, len, [pad])
+
+* `val` - string to align
+* `len` - total length of created string
+* `pad` - padding / fill char (optional, default `' '`)
+
+Example:
+
+```js
+table.alignCenter('hey', 7) // '  hey  '
+```
+
+
+#### AsciiTable.alignRight(val, len, [pad])
+
+* `val` - string to align
+* `len` - total length of created string
+* `pad` - padding / fill char (optional, default `' '`)
+
+Example:
+
+```js
+table.alignRight('hey', 7) // '    hey'
+```
+
+
+#### AsciiTable.alignAuto(val, len, [pad])
+
+Attempt to do intelligent alignment of provided `val`, `String` input will 
+be left aligned, `Number` types will be right aligned.
+
+* `val` - string to align
+* `len` - total length of created string
+* `pad` - padding / fill char (optional, default `' '`)
+
+Example:
+
+```js
+table.align(AsciiAlign.LEFT, 'hey', 7) // 'hey    '
+```
+
+
+#### AsciiTable.arrayFill(len, [val])
+
+Create a new array at the given len, filled with the given value, mainly used internally
+
+* `len` - length of array
+* `val` - fill value (optional)
+
+Example:
+
+```js
+AsciiTable.arrayFill(4, 0) // [0, 0, 0, 0]
+```
+
+### Instance Methods
+
+#### instance.setBorder([edge], [fill], [top], [bottom])
+
+Set the border characters for rendering, if no arguments are passed it will be 
+reset to defaults. If a single `edge` arg is passed, it will be used for all borders.
+
+* `edge` - horizontal edges (optional, default `|`)
+* `fill` - vertical edges (optional, default `-`)
+* `top` - top corners (optional, default `.`)
+* `bottom` - bottom corners (optional, default `'`)
+
+Example:
+
+```js
+var table = new AsciiTable('Stars')
+table
+  .setBorder('*')
+  .setHeading('oh', 'look')
+  .addRow('so much', 'star power')
+
+console.log(table.toString())
+```
+
+```
+************************
+*        Stars         *
+************************
+*   oh    *    look    *
+************************
+* so much * star power *
+************************
+```
+
+
+#### instance.removeBorder()
+
+Example:
+
+```js
+table.removeBorder()
+
+console.log('' + table)
+```
+
+```
+  #     Fruit           Thing
+ --- ----------- --------------------
+  a       apple   Some longer string
+  b      banana           hi
+  c      carrot          meow
+  e   elephants
+```
+
+
+#### instance.setAlign(idx, direction)
+
+* `idx` - column index to align
+* `direction` - alignment direction, (`AsciiAlign.LEFT`, `AsciiAlign.CENTER`, `AsciiAlign.RIGHT`)
+
+Example:
+
+```js
+table
+  .setAlign(2, AsciiAlign.RIGHT)
+  .setAlign(1, AsciiAlign.CENTER)
+
+console.log(table.toString())
+```
+
+```
+.-------------------------------------.
+| a  |   apple   | Some longer string |
+| b  |   banana  |                 hi |
+| c  |   carrot  |               meow |
+| e  | elephants |                    |
+'-------------------------------------'
+```
+
+
+#### instance.setAlignLeft(idx)
+
+Alias to `instance.setAlign(idx, AsciiAlign.LEFT)`
+
+
+#### instance.setAlignCenter(idx)
+
+Alias to `instance.setAlign(idx, AsciiAlign.CENTER)`
+
+
+#### instance.setAlignRight(idx)
+
+Alias to `instance.setAlign(idx, AsciiAlign.RIGHT)`
+
+
+#### instance.setTitle(title)
+
+* `title` - table title
+
+Example:
+
+```js
+var table = new AsciiTable('Old Title')
+
+table.setTitle('New Title')
+```
+
+#### instance.getTitle()
+
+Get the current title of the table
+
+Example:
+
+```js
+table.getTitle() // 'New Title'
+```
+
+
+#### instance.setTitleAlign(direction)
+
+* `direction` - table alignment direction
+
+Example:
+
+```js
+```
+
+
+#### instance.setTitleAlignLeft()
+
+Alias to `instance.setTitleAlign(AsciiAlign.LEFT)`
+
+
+#### instance.setTitleAlignCenter()
+
+Alias to `instance.setTitleAlign(AsciiAlign.CENTER)`
+
+
+#### instance.setTitleAlignRight()
+
+Alias to `instance.setTitleAlign(AsciiAlign.RIGHT)`
+
+
+#### instance.sort(iterator)
+
+* `iterator` - sorting method to run against the rows
+
+Example:
+
+```js
+table.sort(function(a, b) {
+  return a[2] - b[2]
+})
+console.log(table.toString())
+```
+
+```
+.----------------.
+| 2 | John |  34 |
+| 1 | Bob  |  52 |
+| 3 | Jim  |  83 |
+'----------------'
+```
+
+
+#### instance.sortColumn(index, iterator)
+
+Sorting shortcut for targeting a specific column
+
+* `index` - column idx to sort
+* `iterator` - sorting method to run against column values
+
+Example:
+
+```js
+// This is quivalent to the `sort` example above
+table.sortColumn(2, function(a, b) {
+  return a - b
+})
+```
+
+
+#### instance.setHeading(heading, [...])
+
+Set the column headings for the table, takes arguments the same way as `addRow`
+
+* `heading` - heading array or arguments
+
+Example:
+
+```js
+table.setHeading('ID', 'Key', 'Value')
+
+// or:
+
+table.setHeading(['ID', 'Key', 'Value'])
+```
+
+
+#### instance.setHeadingAlign(direction)
+
+* `direction` - 
+
+Example:
+
+```js
+```
+
+
+#### instance.setHeadingAlignLeft()
+
+Alias to `instance.setHeadingAlignLeft(AsciiAlign.LEFT)`
+
+
+#### instance.setHeadingAlignCenter()
+
+Alias to `instance.setHeadingAlignLeft(AsciiAlign.CENTER)`
+
+
+#### instance.setHeadingAlignRight()
+
+Alias to `instance.setHeadingAlignLeft(AsciiAlign.RIGHT)`
+
+
+#### instance.addRow(row, [...])
+
+Rows can be added using a single array argument, or the arguments if multiple 
+args are used when calling the method.
+
+* `row` - array or arguments of column values
+
+Example:
+
+```js
+var table = new AsciiTable()
+
+table
+  .addRow(1, 'Bob', 52)
+  .addRow([2, 'John', 34])
+
+console.log(table.render())
+```
+
+```
+.---------------.
+| 1 | Bob  | 52 |
+| 2 | John | 34 |
+'---------------'
+```
+
+
+#### instance.addRowMatrix(rows)
+
+Bulk `addRow` operation
+
+* `rows` - multidimentional array of rows
+
+Example:
+
+```js
+table.addRowMatrix([
+  [2, 'John', 34]
+, [3, 'Jim', 83]
+])
+
+```
+
+
+#### instance.setJustify(enabled)
+
+Justify all columns to be the same width
+
+* `enabled` - boolean for turning justify on or off, `undefined` considered true
+
+Example:
+
+```js
+table
+  .addRow('1', 'two', 'three')
+  .setJustify()
+
+console.log(table.toString())
+```
+
+```
+.-----------------------.
+| 1     | two   | three |
+'-----------------------'
+```
+
+
+#### instance.toString()
+
+Render the instance as a string for output
+
+**Alias**: [`valueOf`, `render`]
+
+
+#### instance.toJSON()
+
+Return the JSON representation of the table, this also allows us to call 
+`JSON.stringify` on the instance.
+
+Example:
+
+```js
+var table = new AsciiTable('Title')
+
+table
+  .setHeading('id', 'name')
+  .addRow(1, 'Bob')
+  .addRow(2, 'Steve')
+
+console.log(table.toJSON())
+console.log(JSON.stringify(table))
+```
+
+```js
+{
+  title: 'Title'
+, heading: [ 'id', 'name' ]
+, rows: [ 
+    [ 1, 'Bob' ]
+  , [ 2, 'Steve' ] 
+  ] 
+}
+```
+
+```
+{"title":"Title","heading":["id","name"],"rows":[[1,"Bob"],[2,"Steve"]]}
+```
+
+
+#### instance.fromJSON(obj)
+
+Populate the table from json object, should match the `toJSON` output above.
+
+**Alias**: [`parse`]
+
+Example:
+
+```js
+var table = new AsciiTable().fromJSON({
+  title: 'Title'
+, heading: [ 'id', 'name' ]
+, rows: [ 
+    [ 1, 'Bob' ]
+  , [ 2, 'Steve' ] 
+  ] 
+})
+```
+
+
+#### instance.clear()
+
+Clear / reset all table data
+
+**Alias**: [`reset`]
+
+
+#### instance.clearRows()
+
+Reset all row data, maintains title and headings.
+
+
+License
+-------
+
+MIT © [eeve](https://github.com/eeve)
---
+++ mod.ts
@@ -0,0 +1,586 @@
+export interface AsciiTableOptions {
+  prefix?: string;
+}
+
+export interface AsciiData {
+  title: string;
+  heading: string[];
+  rows: any[][];
+}
+
+export enum AsciiAlign {
+  LEFT,
+  CENTER,
+  RIGHT,
+}
+
+export default class AsciiTable {
+  options: AsciiTableOptions;
+
+  private __name = "";
+  private __nameAlign = AsciiAlign.CENTER;
+  private __rows: any[] = [];
+  private __maxCells = 0;
+  private __aligns: number[] = [];
+  private __colMaxes: any[] = [];
+  private __spacing = 1;
+  private __heading: string[] | null = null;
+  private __headingAlign = AsciiAlign.CENTER;
+
+  private __border = true;
+  private __edge = "|";
+  private __fill = "-";
+  private __top = ".";
+  private __bottom = "'";
+
+  private __justify = false;
+
+  constructor(name?: string, options?: AsciiTableOptions) {
+    this.options = options || {};
+    this.reset(name);
+  }
+
+  static fromJSON(obj: AsciiData) {
+    return new AsciiTable().parse(obj);
+  }
+
+  /**
+   * Align the a string at the given length
+   *
+   * @param {Number} direction
+   * @param {String} string input
+   * @param {Number} string length
+   * @param {Number} padding character
+   * @api public
+   */
+  static align(dir: AsciiAlign, str: string, len: number, pad: string = "") {
+    if (dir === AsciiAlign.LEFT) return AsciiTable.alignLeft(str, len, pad);
+    if (dir === AsciiAlign.RIGHT) return AsciiTable.alignRight(str, len, pad);
+    if (dir === AsciiAlign.CENTER) return AsciiTable.alignCenter(str, len, pad);
+    return AsciiTable.alignAuto(str, len, pad);
+  }
+
+  /**
+   * Left align a string by padding it at a given length
+   *
+   * @param {String} str
+   * @param {Number} string length
+   * @param {String} padding character (optional, default '')
+   * @api public
+   */
+  static alignLeft(str: string, len: number, pad: string = "") {
+    if (!len || len < 0) return "";
+    if (str === undefined || str === null) str = "";
+    if (typeof pad === "undefined") pad = " ";
+    if (typeof str !== "string") str = String(str);
+    var alen = len + 1 - str.length;
+    if (alen <= 0) return str;
+    return str + Array(len + 1 - str.length).join(pad);
+  }
+
+  /**
+   * Center align a string by padding it at a given length
+   *
+   * @param {String} str
+   * @param {Number} string length
+   * @param {String} padding character (optional, default '')
+   * @api public
+   */
+  static alignCenter(str: string, len: number, pad: string = "") {
+    if (!len || len < 0) return "";
+    if (str === undefined || str === null) str = "";
+    if (typeof pad === "undefined") pad = " ";
+    if (typeof str !== "string") str = String(str);
+    var nLen = str.length,
+      half = Math.floor(len / 2 - nLen / 2),
+      odds = Math.abs((nLen % 2) - (len % 2)),
+      len = str.length;
+
+    return (
+      AsciiTable.alignRight("", half, pad) +
+      str +
+      AsciiTable.alignLeft("", half + odds, pad)
+    );
+  }
+
+  /**
+   * Right align a string by padding it at a given length
+   *
+   * @param {String} str
+   * @param {Number} string length
+   * @param {String} padding character (optional, default '')
+   * @api public
+   */
+  static alignRight(str: string, len: number, pad: string = "") {
+    if (!len || len < 0) return "";
+    if (str === undefined || str === null) str = "";
+    if (typeof pad === "undefined") pad = " ";
+    if (typeof str !== "string") str = String(str);
+    var alen = len + 1 - str.length;
+    if (alen <= 0) return str;
+    return Array(len + 1 - str.length).join(pad) + str;
+  }
+
+  /**
+   * Auto align string value based on object type
+   *
+   * @param {Any} object to string
+   * @param {Number} string length
+   * @param {String} padding character (optional, default '')
+   * @api public
+   */
+  static alignAuto(str: any, len: number, pad: string = "") {
+    if (str === undefined || str === null) str = "";
+    var type = Object.prototype.toString.call(str);
+    pad || (pad = " ");
+    len = +len;
+    if (type !== "[object String]") {
+      str = str.toString();
+    }
+    if (str.length < len) {
+      switch (type) {
+        case "[object Number]":
+          return AsciiTable.alignRight(str, len, pad);
+        default:
+          return AsciiTable.alignLeft(str, len, pad);
+      }
+    }
+    return str;
+  }
+
+  /**
+   * Fill an array at a given size with the given value
+   *
+   * @param {Number} array size
+   * @param {Any} fill value
+   * @return {Array} filled array
+   * @api public
+   */
+  static arrayFill(len: number, fill: any) {
+    var arr = new Array(len);
+    for (var i = 0; i !== len; i++) {
+      arr[i] = fill;
+    }
+    return arr;
+  }
+
+  reset(name?: string | AsciiData) {
+    this.__name = "";
+    this.__nameAlign = AsciiAlign.CENTER;
+    this.__rows = [];
+    this.__maxCells = 0;
+    this.__aligns = [];
+    this.__colMaxes = [];
+    this.__spacing = 1;
+    this.__heading = null;
+    this.__headingAlign = AsciiAlign.CENTER;
+    this.setBorder();
+
+    if (Object.prototype.toString.call(name) === "[object String]") {
+      this.__name = name as string;
+    } else if (Object.prototype.toString.call(name) === "[object Object]") {
+      this.parse(name as AsciiData);
+    }
+    return this;
+  }
+
+  clear(name = undefined) {
+    return this.reset(name);
+  }
+
+  /**
+   * Set the table border
+   *
+   * @param {String} horizontal edges (optional, default `|`)
+   * @param {String} vertical edges (optional, default `-`)
+   * @param {String} top corners (optional, default `.`)
+   * @param {String} bottom corners (optional, default `'`)
+   * @api public
+   */
+  setBorder(edge = "|", fill = "-", top = ".", bottom = "'") {
+    this.__border = true;
+    if (arguments.length === 1) {
+      fill = top = bottom = edge;
+    }
+    this.__edge = edge || "|";
+    this.__fill = fill || "-";
+    this.__top = top || ".";
+    this.__bottom = bottom || "'";
+    return this;
+  }
+
+  /**
+   * Remove all table borders
+   *
+   * @api public
+   */
+  removeBorder() {
+    this.__border = false;
+    this.__edge = " ";
+    this.__fill = " ";
+    return this;
+  }
+
+  /**
+   * Set the column alignment at a given index
+   *
+   * @param {Number} column index
+   * @param {Number} alignment direction
+   * @api public
+   */
+  setAlign(idx: number, dir: number) {
+    this.__aligns[idx] = dir;
+    return this;
+  }
+
+  /**
+   * Set the title of the table
+   *
+   * @param {String} title
+   * @api public
+   */
+  setTitle(name: string) {
+    this.__name = name;
+    return this;
+  }
+
+  /**
+   * Get the title of the table
+   *
+   * @return {String} title
+   * @api public
+   */
+  getTitle() {
+    return this.__name;
+  }
+
+  /**
+   * Set table title alignment
+   *
+   * @param {Number} direction
+   * @api public
+   */
+  setTitleAlign(dir: number) {
+    this.__nameAlign = dir;
+    return this;
+  }
+
+  /**
+   * AsciiTable sorting shortcut to sort rows
+   *
+   * @param {Function} sorting method
+   * @api public
+   */
+  sort(method: (a: any, b: any) => number) {
+    this.__rows.sort(method);
+    return this;
+  }
+
+  /**
+   * Sort rows based on sort method for given column
+   *
+   * @param {Number} column index
+   * @param {Function} sorting method
+   * @api public
+   */
+  sortColumn(idx: number, method: Function) {
+    this.__rows.sort(function (a, b) {
+      return method(a[idx], b[idx]);
+    });
+    return this;
+  }
+
+  /**
+   * Set table heading for columns
+   *
+   * @api public
+   */
+  setHeading(row: any[] | any, ...args: any[]) {
+    if (
+      args.length > 1 ||
+      Object.prototype.toString.call(row) !== "[object Array]"
+    ) {
+      // row = Array.prototype.slice.call(arguments);
+      row = [row].concat(args);
+    }
+    this.__heading = row;
+    return this;
+  }
+
+  /**
+   * Get table heading for columns
+   *
+   * @return {Array} copy of headings
+   * @api public
+   */
+  getHeading() {
+    return this.__heading ? this.__heading.slice() : [];
+  }
+
+  /**
+   * Set heading alignment
+   *
+   * @param {Number} direction
+   * @api public
+   */
+  setHeadingAlign(dir: AsciiAlign) {
+    this.__headingAlign = dir;
+    return this;
+  }
+
+  /**
+   * Add a row of information to the table
+   *
+   * @param {...|Array} argument values in order of columns
+   * @api public
+   */
+  addRow(row: any | any[], ...args: any[]) {
+    if (
+      args.length > 1 ||
+      Object.prototype.toString.call(row) !== "[object Array]"
+    ) {
+      // row = Array.prototype.slice.call(arguments);
+      row = [row].concat(args);
+    }
+    this.__maxCells = Math.max(this.__maxCells, row.length);
+    this.__rows.push(row);
+    return this;
+  }
+
+  /**
+   * Get a copy of all rows of the table
+   *
+   * @return {Array} copy of rows
+   * @api public
+   */
+  getRows() {
+    return this.__rows.slice().map(function (row) {
+      return row.slice();
+    });
+  }
+
+  /**
+   * Add rows in the format of a row matrix
+   *
+   * @param {Array} row matrix
+   * @api public
+   */
+  addRowMatrix(rows: any[]) {
+    for (var i = 0; i < rows.length; i++) {
+      this.addRow(rows[i]);
+    }
+    return this;
+  }
+
+  /**
+   * Add rows from the given data array, processed by the callback function rowCallback.
+   *
+   * @param {Array} data
+   * @param (Function) rowCallback
+   * @param (Boolean) asMatrix - controls if the row created by rowCallback should be assigned as row matrix
+   * @api public
+   */
+  addData(data: any[], rowCallback: Function, asMatrix: boolean) {
+    if (Object.prototype.toString.call(data) !== "[object Array]") {
+      return this;
+    }
+    for (var index = 0, limit = data.length; index < limit; index++) {
+      var row = rowCallback(data[index]);
+      if (asMatrix) {
+        this.addRowMatrix(row);
+      } else {
+        this.addRow(row);
+      }
+    }
+    return this;
+  }
+
+  /**
+   * Reset the current row state
+   *
+   * @api public
+   */
+  clearRows() {
+    this.__rows = [];
+    this.__maxCells = 0;
+    this.__colMaxes = [];
+    return this;
+  }
+
+  /**
+   * Apply an even spaced column justification
+   *
+   * @param {Boolean} on / off
+   * @api public
+   */
+  setJustify(val = true) {
+    this.__justify = !!val;
+    return this;
+  }
+
+  /**
+   * Convert the current instance to a JSON structure
+   *
+   * @return {Object} json representation
+   * @api public
+   */
+  toJSON() {
+    return {
+      title: this.getTitle(),
+      heading: this.getHeading(),
+      rows: this.getRows(),
+    };
+  }
+
+  /**
+   * Populate the table from a JSON object
+   *
+   * @param {Object} json representation
+   * @api public
+   */
+  parse(obj: AsciiData) {
+    return this.clear()
+      .setTitle(obj.title)
+      .setHeading(obj.heading)
+      .addRowMatrix(obj.rows);
+  }
+
+  /**
+   * Render the table with the current information
+   *
+   * @return {String} formatted table
+   * @api public
+   */
+  render() {
+    var self = this,
+      body = [],
+      mLen = this.__maxCells,
+      max = AsciiTable.arrayFill(mLen, 0),
+      total = mLen * 3,
+      rows = this.__rows,
+      justify = 0,
+      border = this.__border,
+      all = this.__heading ? [this.__heading].concat(rows) : rows;
+
+    // Calculate max table cell lengths across all rows
+    for (let i = 0; i < all.length; i++) {
+      const row = all[i] ?? [];
+      for (let k = 0; k < mLen; k++) {
+        const cell = row[k];
+        max[k] = Math.max(max[k], cell ? cell.toString().length : 0);
+      }
+    }
+    this.__colMaxes = max;
+    justify = this.__justify ? Math.max.apply(null, max) : 0;
+
+    // Get
+    max.forEach(function (x) {
+      total += justify ? justify : x + self.__spacing;
+    });
+    justify && (total += max.length);
+    total -= this.__spacing;
+
+    // Heading
+    border && body.push(this._seperator(total - mLen + 1, this.__top));
+    if (this.__name) {
+      body.push(this._renderTitle(total - mLen + 1));
+      border && body.push(this._seperator(total - mLen + 1));
+    }
+    if (this.__heading) {
+      body.push(
+        this._renderRow(this.__heading ?? [], " ", this.__headingAlign),
+      );
+      body.push(this._rowSeperator(mLen, this.__fill));
+    }
+    for (var i = 0; i < this.__rows.length; i++) {
+      body.push(this._renderRow(this.__rows[i], " "));
+    }
+    border && body.push(this._seperator(total - mLen + 1, this.__bottom));
+
+    var prefix = this.options.prefix || "";
+    return prefix + body.join("\n" + prefix);
+  }
+
+  valueOf() {
+    return this.render();
+  }
+
+  toString() {
+    return this.render();
+  }
+
+  /**
+   * Create a line seperator
+   *
+   * @param {Number} string size
+   * @param {String} side values (default '|')
+   * @api private
+   */
+  private _seperator(len: number, sep?: string) {
+    sep || (sep = this.__edge);
+    return sep + AsciiTable.alignRight(sep, len, this.__fill);
+  }
+
+  /**
+   * Create a row seperator
+   *
+   * @return {String} seperator
+   * @api private
+   */
+  private _rowSeperator(len?: number, fill?: string) {
+    var blanks = AsciiTable.arrayFill(
+      len || this.__maxCells,
+      fill || this.__fill,
+    );
+    return this._renderRow(blanks, this.__fill);
+  }
+
+  /**
+   * Render the table title in a centered box
+   *
+   * @param {Number} string size
+   * @return {String} formatted title
+   * @api private
+   */
+  private _renderTitle(len: number) {
+    var name = " " + this.__name + " ",
+      str = AsciiTable.align(this.__nameAlign, name, len - 1, " ");
+    return this.__edge + str + this.__edge;
+  }
+
+  /**
+   * Render an invdividual row
+   *
+   * @param {Array} row
+   * @param {String} column seperator
+   * @param {Number} total row alignment (optional, default `auto`)
+   * @return {String} formatted row
+   * @api private
+   */
+  private _renderRow(row: any[], str?: string, align?: AsciiAlign) {
+    var tmp = [""],
+      max = this.__colMaxes;
+
+    for (var k = 0; k < this.__maxCells; k++) {
+      var cell = row[k],
+        just = this.__justify ? Math.max.apply(null, max) : max[k],
+        // , pad = k === this.__maxCells - 1 ? just : just + this.__spacing
+        pad = just,
+        cAlign = this.__aligns[k],
+        use = align,
+        method = "alignAuto";
+
+      if (typeof align === "undefined") use = cAlign;
+
+      if (use === AsciiAlign.LEFT) method = "alignLeft";
+      if (use === AsciiAlign.CENTER) method = "alignCenter";
+      if (use === AsciiAlign.RIGHT) method = "alignRight";
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      tmp.push(AsciiTable[method](cell, pad, str));
+    }
+    var front = tmp.join(str + this.__edge + str);
+    front = front.substr(1, front.length);
+    return front + str + this.__edge;
+  }
+}
